bd39db1803ce2cd4014137d7869a470ed1544d57
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2022-08-30 17:08:48 +0200

    Create pdm_wave.js

---
+++ pdm_wave.js
@@ -0,0 +1,77 @@
+
+var bufA = new Int16Array(128);
+var bufB = new Int16Array(128);
+var canWrite = false;
+var buffer = new Int16Array(128);
+
+var sample_rate = 31250;
+
+function onSamples(samples) {
+  if(canWrite) {
+    buffer.set(samples);
+  }  
+  canWrite = false;
+}
+
+Pdm.stop();
+Pdm.uninit();
+// jaune 5 bleu 6
+Pdm.setup({"clock" : 5, "din" : 6, frequency: sample_rate});
+Pdm.init(onSamples, bufA, bufB);
+Pdm.start();
+
+
+setWatch(function() { 
+ Pdm.stop();
+ Pdm.uninit();
+ g.clear();
+ g.flip();
+}, BTN, {edge:"rising", debounce:50, repeat:true});
+
+
+function evaluateFrequency() {
+  
+  var zeroCrossSample = 0;
+  var zeroCrossDelay = 0;
+  var do_break = false;
+  for (i = 1; i < buffer.length; i++) {
+    var previous = buffer[i-1];
+    var current = buffer[i]; 
+    if (previous >= 0 && current < 0) {
+        // evaluate Tzc
+        var oldZeroCross = zeroCrossSample;
+        zeroCrossSample = (i - 1.0) + (previous / (previous - current));
+        if(oldZeroCross > 0) {
+          var newZeroCrossDelay = zeroCrossSample - oldZeroCross;
+          if(zeroCrossDelay < 1) {
+            zeroCrossDelay = newZeroCrossDelay;
+          } else {
+            do_break = true;
+          }
+          // Store measured frequency in Hz
+          if(do_break) {
+            return Math.round(sample_rate / ((zeroCrossDelay + newZeroCrossDelay) / 2.0));
+          }
+          zeroCrossDelay = newZeroCrossDelay;
+        }
+    }
+  }
+  return 0;
+}
+
+function onTimer() {
+  g.clear();
+  g.moveTo(0,32 + (buffer[0] / 10));
+  for (var x=1;x<128 && x < buffer.length ;x++) {
+    g.lineTo(x,32 + (buffer[x] / 10));
+  }
+  g.setFontBitmap();
+  g.drawString("Frequency: " + evaluateFrequency() + " Hz");
+  // Use a large font for the value itself
+  g.flip();
+  canWrite = true;
+}
+
+// Update temperature every 2 seconds
+setInterval(onTimer,500);
+

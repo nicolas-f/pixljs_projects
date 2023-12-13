2e4af4d977d960a8701e3b54dfa5d67092566f13
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2023-12-13 11:03:09 +0100

    add menu

--- sonometer_calibration.js
+++ sonometer_calibration.js
@@ -4,6 +4,23 @@ backlight = 0;
 Pixl.setLCDPower(false);
 var idRefreshInterval = 0;
 var noiseLevel=-26.30;
+var gainCalibration=0.0;
+var bufA = new Int16Array(256);
+var bufB = new Int16Array(256);
+
+var mainmenu = {
+  "" : {
+    "title" : "-- Settings --"
+  },
+  "Backlight On" : function() { LED1.set(); },
+  "Backlight Off" : function() { LED1.reset(); },
+  "Calibration" : {
+    value : gainCalibration,
+    min:-50,max:50,step:0.1,
+    onchange : v => { gainCalibration=v; }
+  },
+  "Exit" : function() { homeScreen(); },
+};
 
 function disableButtons() {
   for(id=0;id<4;id++) {
@@ -24,10 +41,11 @@ function homeScreen() {
   g.setFontAlign(1, -1).drawString("Start-", g.getWidth(), 2);
   g.setFontAlign(1, 1).drawString("Stop-", g.getWidth(), g.getHeight());
   g.setFontAlign(-1, 1).drawString("-On/Off", 0, g.getHeight());
-  g.setFontAlign(-1, -1).drawString("-Calibration", 0, 0);  
+  g.setFontAlign(-1, -1).drawString("-Settings", 0, 0);
   g.setFont("Vector16").setFontAlign(0, 0).drawString(noiseLevel+" dBFS", g.getWidth()/2, g.getHeight()/2);
   g.flip();
   disableButtons();
+  button_watch[0] = setWatch(function() { disableButtons(); Pixl.menu(mainmenu); }, BTN1, {  repeat: true,  edge: 'rising'});
 }
 
 homeScreen();

b34c9342263230ef8eff6717b8ce0a5e5e049c3a
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2024-01-08 16:54:29 +0100

    Update pdm_wave_eco.js

--- pdm_wave_eco.js
+++ pdm_wave_eco.js
@@ -1,38 +1,35 @@
-var Averager = require("Averager").Averager;
-var voltageAverager = new Averager({scale:500});
-
 var bufA = new Int16Array(256);
 var bufB = new Int16Array(256);
-var canWrite = false;
 var buffer = new Int16Array(256);
-
+var spl=-99.0;
+var canWrite = false;
 var sample_rate = 15625;
 
-function onSamples(samples) {
+function onSamples(samples, sumSquared) {
+  "jit";
   if(canWrite) {
     buffer.set(samples);
-  }  
+  }
   canWrite = false;
+  spl=10*Math.log(sumSquared/samples.length/1073741824)/Math.log(10);
 }
 
-Pdm.stop();
-Pdm.uninit();
-// jaune 5 bleu 6
-Pdm.setup({"clock" : 5, "din" : 6, frequency: sample_rate});
-Pdm.init(onSamples, bufA, bufB);
-Pdm.start();
-
-
-setWatch(function() { 
+function doUnInit() {
  Pdm.stop();
  Pdm.uninit();
- g.clear();
- g.flip();
-}, BTN, {edge:"rising", debounce:50, repeat:true});
+}
+
+function doInit() {
+  // jaune->clock bleu->din
+  Pdm.setup({"clock" : 5, "din" : 6, frequency: sample_rate, "lgain": 20});
+  Pdm.init(onSamples, bufA, bufB);
+  Pdm.start();
+}
+
 
 
 function evaluateFrequency() {
-  
+  "jit";
   var zeroCrossSample = 0;
   var zeroCrossDelay = 0;
   var do_break = false;
@@ -61,31 +58,34 @@ function evaluateFrequency() {
   return 0;
 }
 
+
 function onTimer() {
-  voltageAverager.add(analogRead(A0) * E.getAnalogVRef() * 2.213);
   g.clear();
   g.moveTo(0,32 + (buffer[0] / 10));
   for (var x=1;x<128 && x < buffer.length ;x++) {
     g.lineTo(x,32 + (buffer[x] / 10));
   }
   g.setFontBitmap();
-  g.drawString(voltageAverager.series.hours.getCurrent()[voltageAverager.series.hours.lastBucket].toFixed(2) + " V - Frequency: " + evaluateFrequency() + " Hz");
+  g.drawString(spl.toFixed(1) + " dB - Frequency: " + evaluateFrequency() + " Hz");
   // Use a large font for the value itself
   g.flip();
   canWrite = true;
+  tm = setTimeout(onTimer, 250);
 }
 
+var tm = 0;
 
-var tm = setInterval(onTimer,500);
-    
 setWatch(function() {
   if(tm == 0) {
+    doInit();
     Pixl.setLCDPower(true);
-    // Update temperature every 2 seconds
-    tm = setInterval(onTimer,500);
+    tm = setTimeout(onTimer, 250);
   } else {
-    clearInterval(tm);
+    clearTimeout(tm);
     tm = 0;
     Pixl.setLCDPower(false);
+    doUnInit();
   }
-}, BTN2, {edge:"rising", debounce:50, repeat:true});
+}, BTN, {edge:"rising", debounce:50, repeat:true});
+
+

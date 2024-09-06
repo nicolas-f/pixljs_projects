e6147fec96ad7d1323bbc588ea885b845cc9dc82
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2024-09-06 15:13:04 +0200

    Update ble_battery_a0.js
    display cpu temp

--- ble_battery_a0.js
+++ ble_battery_a0.js
@@ -1,25 +1,27 @@
 var Averager = require("Averager").Averager;
 var voltageAverager = new Averager({scale:500});
 
-function onSecond() {
+function onTimer() {
   voltageAverager.add(analogRead(A0) * E.getAnalogVRef() * 2.213);
-  volts = voltageAverager.series.hours.getCurrent()[voltageAverager.series.hours.lastBucket];
+  let volts = voltageAverager.series.hours.getCurrent()[voltageAverager.series.hours.lastBucket];
   NRF.setAdvertising([
-      {0x180F : [Math.round(Math.min(1, (volts - 3.2) / (4.22 - 3.2))  * 100)]},
-    ]);
-  // If graphics is defined
-  if (global.g) {
-    g.clear(1);
-    var txt = volts.toFixed(2) + "V";
-    var fontSize = g.getHeight()-8;
-    g.setFontAlign(0,0);
-    g.drawString("Battery Voltage",g.getWidth()/2,4);
-    g.setFontVector(fontSize);
-    while (g.stringWidth(txt) > g.getWidth()) {
-      g.setFontVector(--fontSize);
-    }
-    g.drawString(txt, g.getWidth()/2, 4+g.getHeight()/2);
-    g.flip();
-  }
+    {0x180F : [Math.round(Math.min(1, (volts - 3.2) / (4.22 - 3.2))  * 100)]},
+  ]);
+  // Get the temperature as a string
+  var t = E.getTemperature().toFixed(1);
+  // Clear display
+  g.clear();
+  // Use the small font for a title
+  g.setFontBitmap();
+  g.drawString(volts.toFixed(2)+" V - Temperature:");
+  // Use a large font for the value itself
+  g.setFontVector(40);
+  g.drawString(t, (g.getWidth()-g.stringWidth(t))/2,10);
+  // Update the screen
+  g.flip();
 }
-setInterval(onSecond, 5000);
+
+// Update temperature every 2 seconds
+setInterval(onTimer,5000);
+// Update temperature immediately
+onTimer();
